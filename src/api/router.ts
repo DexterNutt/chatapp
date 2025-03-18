ecf59f7e70c297edcf597c01bdd6f7c4f62e2cfd
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2025-03-18 15:47:16 +0100

    Add routing and app init

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,4 +1,14 @@
 import { Hono } from "hono";
+import { ctxMiddleware } from "../lib/hono";
+import { authRouter } from "./auth/router";
+import { userRouter } from "./user/router";
+import { chatRouter } from "./chat/router";
+import { logger } from "hono/logger";
 
 export const apiBaseEndpoint = "/api";
-export const apiRouter = new Hono();
+
+export const apiRouter = new Hono().use(ctxMiddleware).use(logger());
+
+apiRouter.route("/auth", authRouter);
+apiRouter.route("/user", userRouter);
+apiRouter.route("/chat", chatRouter);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
 import { Hono } from "hono";
 import { apiRouter } from "./api/router";
+import { AppError } from "./lib/error";
+import { streamSSE } from "hono/streaming";
+import { z } from "zod";
+import { errorWrapperSchema } from "./lib/zod";
 
 export const app = new Hono();
 
+const port = process.env["APP_PORT"];
+
+app.get("/", (c) => c.text("Server is running 🚀"));
+
 app.route("/api", apiRouter);
+
+app.onError(async (err, c) => {
+    console.error(c.req.method, c.req.url, err);
+
+    const error = AppError.from(err);
+    const fieldErrors =
+        error.code === "BAD_REQUEST" && error.cause instanceof z.ZodError
+            ? error.cause.flatten().fieldErrors
+            : undefined;
+
+    const output = await errorWrapperSchema.parseAsync({
+        message:
+            Object.values(fieldErrors ?? {})
+                .at(0)
+                ?.at(0) ?? error.message,
+        fieldErrors,
+    });
+
+    if (c.req.header("Accept") === "text/event-stream") {
+        return streamSSE(c, (stream) =>
+            stream.writeSSE({
+                id: crypto.randomUUID(),
+                event: "error",
+                data: JSON.stringify(output),
+            })
+        );
+    }
+
+    return c.json(output, error.httpCode());
+});
+
+export default {
+    fetch: app.fetch,
+    idleTimeout: 0,
+    port,
+};
